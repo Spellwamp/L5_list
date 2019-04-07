5be716bbac51a92c0f80624130d343e3346c968f
Spellwamp <44416431+Spellwamp@users.noreply.github.com>
2019-04-07 17:31:59 +0300

    Update task_1_4.py

--- task_1_4.py
+++ task_1_4.py
@@ -16,3 +16,26 @@ for i in range(len(matrix)):
 
 pp = pprint.PrettyPrinter(width=len(str(matrix)) - 1)
 pp.pprint(matrix_finaly)
+
+
+
+# import pprint
+# from random import randint
+
+# a, b = int(input()), int(input())
+# matrix = [[randint(0, 20) for i in range(a)]for j in range(b)]
+# matrix1 = [[randint(0, 20) for i in range(a)]for j in range(b)]
+# matrix_finaly = []
+
+# for i in range(len(matrix)):
+#     temp = []
+#     for j in range(len(matrix)):
+#         if matrix[i][j] >= matrix1[i][j]:
+#             temp.append(matrix[i][j])
+#         else: temp.append(matrix1[i][j])
+#     matrix_finaly.append(temp)
+
+# pp = pprint.PrettyPrinter(width=len(str(matrix)) - 1)
+# pp.pprint(matrix)
+# pp.pprint(matrix1)
+# pp.pprint(matrix_finaly)
